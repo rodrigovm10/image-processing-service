3189c1f55acbc8a4a1539ebfcf9c58d35ee17124
Rodrigo Vega <rodrigovmdev10@gmail.com>
2025-04-13 07:29:39 -0600

    feat: Add database connection

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import { envs } from './config/envs'
+import { MongoDatabase } from './data/mongo'
 import { AppRoutes } from './presentation/routes'
 import { Server } from './presentation/server'
 ;(() => {
   main()
 })()
 
-function main() {
+async function main() {
+  await MongoDatabase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL,
+  })
+
   const server = new Server({
     port: envs.PORT,
     routes: AppRoutes.routes,
